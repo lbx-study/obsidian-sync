e7bc7a5d3d4839269dce294680130729cfca3094
LBX <lbx.study@proton.me>
2025-01-20 16:19:54 +0530

    vault backup: 2025-01-20 16:19:54

---
+++ C/area.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void) {
+
+int l,b;
+
+printf("Enter the length:\n");
+scanf("%d", &l);
+
+printf("Enter the breadth:\n");
+scanf("%d", &b);
+
+printf("The area of rectangle is %d\n", l*b);
+
+return 0;
+}
---
+++ C/user.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+// Creating main function
+
+int main() {
+
+// declare the varibles
+
+int age;
+char gen[10];
+
+printf("Enter values\n");
+
+// Taking user input
+
+scanf("%d, %c", &age, gen);
+
+// Print the values
+
+printf("The values are %d and %c \n", age, gen);
+
+
+return 0;
+}
